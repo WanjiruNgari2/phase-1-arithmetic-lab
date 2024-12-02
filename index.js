b475dc480c5b84f3d7c5cf24dbc5db8e3bcf1c8d
Damaris Ngari <wanjirungari2@gmail.com>
2024-12-02 21:09:35 +0300

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
 
-// Write your code here
+console.log(multiply);
+
+
+//  a random integer greater than 0
+let random = Math.floor(Math.random() * 100) + 1; 
+
+//mod
+let num3 = 28;
+let num4 = 6;
+let mod = num3 % num4;
+
+console.log(mod);
+
+
+//max
+let max = Math.max (14,18,19,20);
+console.log(max);
+
+
+
+
+
